af767287078e393e676ba11983f4e87836fa2868
ymizushi <mizushi@gmail.com>
2014-02-09 01:17:13 +0900

    edit setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+      name = "nicosearch",
+      version = "0.0.1",
+      license = open('LICENSE').read(),
+      platforms = ['POSIX'],
+      description = "https://github.com/ymizushi/nicosearch",
+      author = "ymizushi",
+      author_email = "mizushi@gmail.com",
+      url = "https://github.com/ymizushi/nicosearch",
+      keywords = ["search", "niconico"],
+      classifiers = [
+                     'License :: OSI Approved :: MIT License',
+                     "Programming Language :: Python",
+                     "Development Status :: 4 - Beta",
+                     "Environment :: Console",
+                     "Intended Audience :: Developers",
+                     "Topic :: Utilities",
+                     "Topic :: Software Development",
+                     ],
+      long_description = open('README.md').read()
+      )
